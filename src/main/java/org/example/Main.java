eb4618d34b6566f63e31f8c3d4bdeaa42645bfdf
anthem53 <dlwlgus0231@naver.com>
2022-12-08 18:55:50 +0900

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello worl555d!");
+    }
+}
