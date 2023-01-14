852c3013aa64f6eaedac0dc5b0d0ce1f341dd378
Lee jihyeon <anthem53@postech.ac.kr>
2023-01-14 15:33:16 +0900

    add security for guest can access normal board content

--- src/main/java/com/test/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/test/springboot/config/auth/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.test.springboot.config.auth;
 
 import com.test.springboot.domain.user.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,19 +17,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                    .csrf().disable()
-                    .headers().frameOptions().disable()
-                .and()
-                    .authorizeRequests()
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
-                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
-                    .anyRequest().authenticated()
-                .and()
-                    .logout()
-                    .logoutSuccessUrl("/")
-                .and()
-                    .oauth2Login()
-                    .userInfoEndpoint()
-                    .userService(customOAuth2UserService);
+            .csrf().disable()
+            .headers().frameOptions().disable()
+        .and()
+            .authorizeRequests()
+            .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+            .antMatchers("/", "/posts/inquiry/**", "/login").permitAll() // 설정된 url은 인증되지 않더라도 누구든 접근 가능
+            .anyRequest().authenticated()
+        .and()
+            .logout()
+            .logoutSuccessUrl("/")
+        .and()
+            .oauth2Login()
+            .userInfoEndpoint()
+            .userService(customOAuth2UserService);
     }
 }
